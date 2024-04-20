894fe4bb5fee394c09545233c20da16e7a79cf37
deepthinidwannaya <deepthi.nidv@ymail.com>
2024-04-20 19:01:24 +0530

    Add module for vpc

--- main.tf
+++ main.tf
@@ -18,11 +18,42 @@ data "aws_vpc" "default" {
   default = true
 }
 
+module "blog_sec_grp" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.1.2"
+  name = "blog_new"
+
+  vpc_id = module.blog_vpc.vpc_id
+
+  ingress_rules = ["http-80-tcp", "https-443-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
+module "blog_vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "dev"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-west-2a", "us-west-2b", "us-west-2c"]
+  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24", "10.0.103.0/24"]
+
+  tags = {
+    Terraform = "true"
+    Environment = "dev"
+  }
+}
+
 resource "aws_instance" "blog" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
   vpc_security_group_ids = [module.blog_sec_grp.security_group_id]
+
+  subnet_id = module.blog_vpc.public_subnets[0]
 
   tags = {
     Name = "HelloWorld"
@@ -33,19 +64,6 @@ data "aws_vpc" "blog" {
   default = true
 }
 
-module "blog_sec_grp" {
-  source  = "terraform-aws-modules/security-group/aws"
-  version = "5.1.2"
-  name = "blog_new"
-
-  vpc_id = data.aws_vpc.default.id
-
-  ingress_rules = ["http-80-tcp", "https-443-tcp"]
-  ingress_cidr_blocks = ["0.0.0.0/0"]
-
-  egress_rules = ["all-all"]
-  egress_cidr_blocks = ["0.0.0.0/0"]
-}
 
 
 #resource "aws_security_group" "blog" {
