c7d5f93103c1c2a861e964b6b2d4eb351a0f17c4
deepthinidwannaya <deepthi.nidv@ymail.com>
2024-04-20 19:11:34 +0530

    Add alb

--- main.tf
+++ main.tf
@@ -53,7 +53,7 @@ module "alb" {
   name    = "blog-alb"
   vpc_id  = module.blog_vpc.vpc_id
   subnets = module.blog_vpc.public_subnets
-  security_groups = module.blog_sec_grp.security_group_id
+  security_groups = [module.blog_sec_grp.security_group_id]
 
   listeners = {
     ex-http-https-redirect = {
@@ -73,12 +73,7 @@ module "alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id = aws_instance.blog.id
-          port = 80
-        }
-      }
+      target_id = aws_instance.blog.id
     }
   }
 
