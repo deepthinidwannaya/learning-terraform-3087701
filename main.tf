4163d7c46e573ea217f43a7b31afd17787d42a07
deepthinidwannaya <deepthi.nidv@ymail.com>
2024-04-22 10:45:14 +0530

    Add auto-scaling

--- main.tf
+++ main.tf
@@ -14,6 +14,7 @@ data "aws_ami" "app_ami" {
   owners = ["979382823631"] # Bitnami
 }
 
+
 data "aws_vpc" "default" {
   default = true
 }
@@ -73,7 +74,6 @@ module "blog_alb" {
       protocol         = "HTTP"
       port             = 80
       target_type      = "instance"
-#      target_id = aws_instance.blog.id
     }
   }
 
